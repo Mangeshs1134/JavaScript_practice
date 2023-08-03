56690d1da1073a7123cc07db999769d72dc6c3bf
Mangeshs1134 <63158834+Mangeshs1134@users.noreply.github.com>
2023-08-03 22:38:56 +0530

    Add files via upload

---
+++ day 2/for_in.js
@@ -0,0 +1,40 @@
+    // for in loop
+
+    const obj =  {
+    A : B ,
+    B: C ,
+    D : E 
+
+    }
+
+// for in loop
+
+const obj =  {
+    A : "F" ,
+    B: "C ",
+    D : "E" 
+ 
+ }
+ 
+//  for (let b in obj) {    NOT ITERABLE
+
+// obj2 = "mangesh";
+ for (let b of 'mangesh') {
+     // if (Object.hasOwnProperty.call(object, key)) {
+     //     const element = object[key];
+         
+     // }
+     console.log(b)
+ }
+
+    //FOR of
+
+
+  // obj2 = "mangesh";
+
+
+ for (let b of 'mangesh') {
+  
+  
+     console.log(b)
+ }
