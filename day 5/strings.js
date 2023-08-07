810a2555af7e39149fddcd3f4415b8f7fc241d69
Mangeshs1134 <63158834+Mangeshs1134@users.noreply.github.com>
2023-08-07 23:40:54 +0530

    Add files via upload

---
+++ day 5/strings.js
@@ -0,0 +1,15 @@
+//strings example
+const name = 'Peter';
+const name1 = "Jack";
+const result = `The names are ${name} and ${name1}`;
+
+// example of index
+
+let a = 'hello';
+a[0] = 'H';
+console.log(a); // "hello"
+
+// JS is case sensitive
+const a = 'a';
+const b = 'A'
+console.log(a === b); // false
---
+++ day 5/strings_02.js
@@ -0,0 +1,11 @@
+
+// objects in JS
+
+const a = 'hello';
+const b = new String('hello');
+
+console.log(a); // "hello"
+console.log(b); // "hello"
+
+console.log(typeof a); // "string"
+console.log(typeof b); // "object"
